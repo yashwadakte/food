314fdf7c4018972928c551705649e2e90fb4b73b
yashwadakte <102813870+yashwadakte@users.noreply.github.com>
2024-10-29 10:19:08 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <section className="home" id="home">
+         <div className="content">
+            <h3>
+                fresh <span>food in the</span>morning
+            </h3>
+            <p>
+            A restaurant captain is responsible for managing the dining experience for guests and ensuring that the restaurant runs smoothly
+            </p>
+            <a href="#" className="btn">
+                get yours now
+            </a>
+         </div>
+
+    </section>
+    </>
+  )
+}
+
+export default Home
